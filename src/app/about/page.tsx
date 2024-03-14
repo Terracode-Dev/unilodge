44a206265edc94c6a4867e60aaf5bd6a6a13b9b6
navindu-sachintha <navindu.social@gmail.com>
2024-03-14 06:24:48 +0530

    Fe: Add Profile Cards

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,33 @@
+import ProfileCard from '@/components/Profilecard'
 import React from 'react'
 
 type Props = {}
 
 export default function About({}: Props) {
+  const navindu = {
+    name:'Navindu',
+    imageUrl: 'https://i.ibb.co/jrt7KD6/default-profile.png'
+  }
+  const nomin = {
+    name:'Nomin',
+    imageUrl: 'https://i.ibb.co/jrt7KD6/default-profile.png'
+  }
+  const pasindu = {
+    name:'Pasindu',
+    imageUrl: 'https://i.ibb.co/jrt7KD6/default-profile.png'
+  
+  }
+  const chamod = {
+    name:'Chamod',
+    imageUrl: 'https://i.ibb.co/jrt7KD6/default-profile.png'
+  
+  }
   return (
-    <div>
-      About
+    <div className="flex justify-center items-center h-screen gap-2">
+      <ProfileCard {...navindu} />
+      <ProfileCard {...nomin} />
+      <ProfileCard {...pasindu} />
+      <ProfileCard {...chamod} />
     </div>
   )
 }
