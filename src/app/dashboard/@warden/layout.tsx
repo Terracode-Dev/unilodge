5cb6ed9512c1ca218ac77660d05fea3e6c8036e2
navindu-sachintha <navindu.social@gmail.com>
2024-03-17 22:05:01 +0530

    En: Added tab pages to wardem

--- src/app/dashboard/@warden/layout.tsx
+++ src/app/dashboard/@warden/layout.tsx
@@ -3,11 +3,23 @@ import Link from 'next/link'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <nav>
-        <Link href="/dashboard/pending">Pending</Link>
-        <Link href="/dashboard/approved">Accepted</Link>
-        <Link href="/dashboard/rejected">Rejected</Link>
-      </nav>
+      <nav className="flex">
+      <Link href="/dashboard/pending" legacyBehavior passHref>
+        <a className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 border border-gray-300 rounded-l">
+          Pending
+        </a>
+      </Link>
+      <Link href="/dashboard/approved" legacyBehavior passHref>
+        <a className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 border border-l-0 border-gray-300">
+          Accepted
+        </a>
+      </Link>
+      <Link href="/dashboard/rejected" legacyBehavior passHref>
+        <a className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 border border-l-0 rounded-r border-gray-300">
+          Rejected
+        </a>
+      </Link>
+    </nav>
       <div>{children}</div>
     </>
   )
