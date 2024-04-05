38d12371740b8318a05d726956f4dc0897744898
Umayanga12 <umayangaathapaththu05@gmail.com>
2024-04-05 09:36:54 +0530

    admin controller

--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -1,8 +1,25 @@
 const pool = require('../db');
 
 // Cretae a new property
-async function createProperty(price, address, lat, lng, picture, name, lid){
-    const client = await pool.connect();
-
+async function createProperty(price, address, lat, lng, picture, name, lid,discription){
+    try {
+        const client = await pool.connect();
+        await client.query(
+            'INSERT INTO property(price, address, lat, lng, picture, name, lid,discription) values($1,$2,$3,$4,$5,$6,$7,$8)',
+            [price,address,lat,lng,picture,name,lid,discription]
+        );
+        await client.query('COMMIT');
+        
+    } catch (error) {
+        await client.query('ROLLBACK');
+        throw error;
+    }
     
 }
+
+//edit property 
+async function edtiPropety(){
+
+}
+
+//delete property
--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,73 +1,23 @@
 const pool = require('../db');
 
-async function createStudent(name, username, email, password){
-    const client = await pool.connect();
-
-    try {
-        await client.query('BEGIN');
-
-        //INSERT INTO users
-        const newUser = await client.query(
-            'INSERT INTO users (name, username, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
-            [name, username, email, password]
-        )
-        //Get user id
-        const userId = newUser.rows[0].userid;
-
-        //INSERT INTO students
-        const newStudent = await client.query(
-            'INSERT INTO student (userid) VALUES ($1) RETURNING *',
-            [userId]
-        )
-
-        //COMMIT TRANSACTION
+async function AddReservation(studentid,propertyid,description){
+    try{
+        const client = await pool.connect();
+        await client.query("BEGIN");
+        const newReservation = await client.query(
+            //QUERY
+            'INSERT INTO reservation (propid,studentid,description) VALUES($1,$2,$3)',
+            [propertyid,studentid,description]
+        );
+        
         await client.query('COMMIT');
 
-        return {
-            user: {
-                ...newUser.rows[0],name, username, email
-            },
-            student: newStudent.rows[0] 
-        }
-    } catch (error) {
+    }catch (error){
         await client.query('ROLLBACK');
         throw error;
-    }finally{
-        client.release();
     }
 }
 
-async function deleteStudent(userId) {
-    const client = await pool.connect();
+module.exports = AddReservation
 
-    try {
-        await client.query('BEGIN');
 
-        // DELETE FROM landlord
-        await client.query(
-            'DELETE FROM student WHERE userid = $1',
-            [userId]
-        );
-
-        // DELETE FROM users
-        await client.query(
-            'DELETE FROM users WHERE userid = $1',
-            [userId]
-        );
-
-        // COMMIT TRANSACTION
-        await client.query('COMMIT');
-
-        return 'Student deleted successfully';
-    } catch (error) {
-        await client.query('ROLLBACK');
-        throw error;
-    } finally {
-        client.release();
-    }
-}
-
-module.exports = {
-    createStudent,
-    deleteStudent
-}
