f4521558d253b54d3127e80eeed75e0b535b37dd
Umayanga12 <umayangaathapaththu05@gmail.com>
2024-04-06 17:25:29 +0530

    warden routing

--- backend/routes/wardenRoutes.js
+++ backend/routes/wardenRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const wardenController = require('../controllers/wardenController');
+const wardenactrion =  require('../controllers/actioncontrollers/wardenaction');
 const bcrypt = require('bcrypt');
 
 router.post('/signup',async (req,res) => {
@@ -21,14 +22,83 @@ router.post('/signup',async (req,res) => {
     }
 })
 
-router.delete('/delete/:userid', async (req, res) => {
+router.delete('/deletewarden/:userid', async (req, res) => {
     try {
         await wardenController.deleteWarden(req.params.userid);
         res.send('Warden deleted');
         res.status(204).end();
     } catch (error) {
-        console.error("Error deleting student", error);
-        res.status(500).send("Error deleting student");
+        console.error(error);
+        res.status(500).send("Error deleting warden");
     }
 })
+
+router.post('searchproperty/:propid',async () =>{
+    try {
+        const propertyid = req.params.propid;
+        const propertydetail = await wardenactrion.searchPropDetail(propertyid);
+        res.status(200).json(propertydetail);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    }
+});
+
+router.post('approveProperty/:propertyid/:wardenid',async (req ,res) =>{
+    try {
+        const propertyid = req.params.propertyid;
+        const wardern = req.params.wardenid;
+        await wardenactrion.Approveprop(propertyid,wardern);
+        res.status(200).send("Approved Successfully");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    }
+});
+
+router.post('rejectProperty/:propertyid/:wardenid',async (req ,res) =>{
+   try {
+        const propertyid = req.params.propertyid;
+        const wardern = req.params.wardenid;
+        await wardenactrion.RejectProp(propertyid,wardern);
+        res.status(200).send("Rejected Successfully");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    } 
+});
+
+
+
+router.post('getallpendingProperty',async (req ,res) =>{
+    try {
+        const pendingprop = await wardenactrion.getAllpending();
+        res.status(200).json(pendingprop);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    }
+});
+
+router.post('getallapprovedproperty',async (req ,res) =>{
+   try {
+        const approvedprop = await wardenactrion.getAllApprovedProperties();
+        res.status(200).json(approvedprop);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    } 
+});
+
+router.post('getllrejecedproperty',async (req ,res) =>{
+    try {
+        const rejectedprop = await wardenactrion.getAllRejectedProperties();
+        res.status(200).json(rejectedprop);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    }    
+});
+
+
 module.exports = router;
