a37d27fbe940816560aa9dc3bfc6fcd5b349b66e
Nomin Sendinu <soloriderns@gmail.com>
2024-04-06 14:46:25 +0530

    Add: map shower to more details screen

--- frontend/src/pages/MoreDetails.jsx
+++ frontend/src/pages/MoreDetails.jsx
@@ -1,5 +1,7 @@
+'use client';
 import React from 'react';
-import MapContainer from '../components/Map';
+import { useEffect } from 'react';
+import { MapShower } from '../utils/mapService';
 const MoreDetails = () => {
   // Sample property details
   const propertyDetails = {
@@ -10,7 +12,38 @@ const MoreDetails = () => {
     area: '2000 sqft',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor lectus nec lorem gravida, nec bibendum mi ullamcorper.',
     image: 'https://via.placeholder.com/800x400/FF5733/FFFFFF/?text=Property+Image',
+
+    //methanata add kale ah methanata load krpn en data ek click krm
+    latitude : "6.821919023919709",
+    longitude : "80.04181147862813"
   };
+
+  useEffect(() => {
+    const mapPart = new MapShower();
+    let mapDiv = document.getElementById('map');
+    if (!mapDiv) {
+      // Perhaps handle the error or retry logic
+      console.log("there is no any element with id - map")
+      return;
+    }
+  
+    const exec = async () => {
+      await mapPart.start(mapDiv);
+      
+        
+        const marker = {
+          name: propertyDetails.title,
+          func: () => { alert("Marker clicked!"); },
+          coordinates: { lat: Number(propertyDetails.latitude), long: Number(propertyDetails.longitude) },
+        };
+  
+        mapPart.addMarker(marker);
+        
+      
+    };
+  
+    exec();
+  }, []);
 
   return (
     <div className="container mx-auto p-4  flex">
@@ -33,8 +66,8 @@ const MoreDetails = () => {
           <p className="text-lg mb-4">Description: {propertyDetails.description}</p>
         </div>
       </div>
-      <div className="bg-white rounded-lg shadow-md p-4">
-        <MapContainer />
+      <div id = "map" className="bg-white shadow-md p-4 h-[600px] w-[600px]">
+        
         </div>
     </div>
   );
