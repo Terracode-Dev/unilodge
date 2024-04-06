9e7b7569bae9e4036cf1388e2f66e39463455417
navindu-sachintha <navindu.social@gmail.com>
2024-04-06 19:05:12 +0530

    Er:NULL return

--- frontend/src/components/PropertiesLandlord.jsx
+++ frontend/src/components/PropertiesLandlord.jsx
@@ -15,25 +15,26 @@ const Properties = () => {
   
     const fetchProperties = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/property/${userid}`);
+        const response = await fetch(`http://localhost:3000/property/${String(userid)}`);
         if (response.ok) {
           const data = await response.json();
-          properties = (data.properties);
+          properties = (data);
         } else {
           throw new Error('Failed to fetch data');
         }
       } catch (error) {
         console.error(error);
       }
-    };
-  
+    };  
+    console.log(properties);
+    console.log(userid);
     return (
       <div className="container mx-auto py-8">
         <h1 className="text-3xl font-bold mb-4">Properties</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 overflow-y-auto max-h-screen">
-          {properties.map(property => (
+          {/*{properties.map(property => (
             <PropertyCard key={property.id} property={property} />
-          ))} 
+          ))} */}
         </div>
       </div>
     );
